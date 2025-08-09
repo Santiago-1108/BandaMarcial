5d73c52649c9c4c0eaac30791e8639e729765922
Santiago Casso <eynercasso@unicomfacauca.edu.co>
2025-08-08 22:42:39 -0500

    configuracion final

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ class Database {
     private $host = 'localhost';
     private $db_name = 'banda_marcial';
     private $username = 'root';
-    private $password = '1108';
+    private $password = '';
     private $conn;
 
     public function getConnection() {
